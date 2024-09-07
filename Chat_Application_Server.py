fb442a98f5a8c4bd94409ad7f82608fc7772f6e8
Saikat Rana <149765443+Saikatriki2004@users.noreply.github.com>
2024-09-07 13:17:09 +0530

    Chat_Application_Server.

--- Chat_Application_Server.py
+++ Chat_Application_Server.py
@@ -1,53 +1,33 @@
-#Server
-import threading
+#server
 import socket
-host = '127.0.0.1'
-port = 59000
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen()
-clients = []
-aliases = []
+import threading
 
-
-def broadcast(message):
-    for client in clients:
-        client.send(message)
-
-# Function to handle clients'connections
-
-
-def handle_client(client):
+def handle_client(client_socket):
     while True:
         try:
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            alias = aliases[index]
-            broadcast(f'{alias} has left the chat room!'.encode('utf-8'))
-            aliases.remove(alias)
+            message = client_socket.recv(1024).decode('utf-8')
+            if message:
+                print(f"Client: {message}")
+                response = input("You: ")
+                client_socket.send(response.encode('utf-8'))
+            else:
+                break
+        except ConnectionResetError:
             break
-# Main function to receive the clients connection
+    
+    client_socket.close()
 
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('0.0.0.0', 9999))
+    server.listen(1)
+    print("Server listening on port 9999...")
 
-def receive():
-    while True:
-        print('Server is running and listening ...')
-        client, address = server.accept()
-        print(f'connection is established with {str(address)}')
-        client.send('alias?'.encode('utf-8'))
-        alias = client.recv(1024)
-        aliases.append(alias)
-        clients.append(client)
-        print(f'The alias of this client is {alias}'.encode('utf-8'))
-        broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
-        client.send('you are now connected!'.encode('utf-8'))
-        thread = threading.Thread(target=handle_client, args=(client,))
-        thread.start()
+    client_socket, addr = server.accept()
+    print(f"Accepted connection from {addr}")
 
+    client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+    client_handler.start()
 
 if __name__ == "__main__":
-    receive()
+    main()
